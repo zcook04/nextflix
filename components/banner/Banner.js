2e948ddbd0edbaad8c024188feca0714d9fd1e9a
Zachary Cook <zcook@ZC-MBAIR.local>
2022-02-21 21:58:21 -0500

    Add hero and apply initial css to hero component.

---
+++ components/banner/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './Banner.module.css'
+import Image from 'next/image'
+
+function Banner(props) {
+    const { title, imgUrl } = props
+    return (
+        <div className={styles.container}>
+            <div className={styles.leftWrapper}>
+                <h3 className={styles.title}>{title}</h3>
+                <div className={styles.buttonWrapper}>
+                    <button className={styles.buttonPrimary}>
+                        <Image src='/static/icons/play_icon.svg' width='40px' height='40px' />Play
+                    </button>
+                    <button className={styles.buttonSecondary}>
+                        <Image src='/static/icons/info_icon.svg' width='30px' height='30px' />More Info
+                    </button>
+                </div>
+            </div>
+            <div className={styles.image} style={{ backgroundImage: `url(${imgUrl})`, width: "100%", height: "100%", backgroundSize: "cover", backgroundPosition: "50% 50%", position: 'absolute', top: '0' }}></div>
+            <div className={styles.overlay}></div>
+        </div>
+    )
+}
+
+export default Banner
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+import Banner from '../components/banner/Banner'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Nextflix</h1>
+      <Banner title="The Witcher" subtitle="Season 2" imgUrl="static/thewitcher.jpeg" />
     </div>
   )
 }
