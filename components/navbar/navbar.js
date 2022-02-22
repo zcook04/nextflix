324121644c2cf73bbbc5fa38a1a71e85843ef771
Zachary Cook <zcook@ZC-MBAIR.local>
2022-02-21 23:25:17 -0500

    Add image to navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,30 +1,49 @@
 import React, { useState } from 'react'
-import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import styles from './navbar.module.css'
 
 
 
 function navbar() {
+    const router = useRouter()
     const [loggingOut, setLoggingOut] = useState(false)
 
-    const usernameHandler = () => {
+    const handleClickUsername = () => {
         loggingOut ? setLoggingOut(false) : setLoggingOut(true)
     }
+
+    const handleClickHome = (e) => {
+        e.preventDefault()
+        router.push("/")
+    }
+
+    const handleClickMyList = (e) => {
+        e.preventDefault()
+        router.push("/browse/my-list")
+    }
+
+    const handleClickLogout = (e) => {
+        e.preventDefault()
+        router.push("/logout")
+    }
+
     return (
         <header className={styles.container}>
-            <div className={styles.navLeft}><span className={styles.logo}>Nextflix</span></div>
+            <div className={styles.navLeft}><span className={styles.logo} onClick={handleClickHome}>Nextflix</span></div>
             <nav className={styles.navMiddle}>
                 <ul className={styles.navItems}>
-                    <li className={styles.navItem}>Home</li>
-                    <li className={styles.navItem}>My Shows</li>
+                    <li className={styles.navItem} onClick={handleClickHome}>Home</li>
+                    <li className={styles.navItem} onClick={handleClickMyList}>My List</li>
                 </ul>
             </nav>
             <div className={styles.navRight}>
-                <div className={styles.username} onClick={usernameHandler}>
+                <div className={styles.username} onClick={handleClickUsername}>
                     zack@zack-cook.com
+                    <Image src='/static/icons/show_more.svg' height="12px" width="30px" alt="Show logout drop-down button" />
                 </div>
-                {loggingOut && <div className={styles.logout}>
-                    <Link href='/logout'>Logout</Link>
+                {loggingOut && <div className={styles.logout} onClick={handleClickLogout}>
+                    Logout
                 </div>}
 
             </div>
