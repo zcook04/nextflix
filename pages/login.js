22db23833dcd86f98bc09f844c600eb1e36c3425
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-22 15:48:25 -0500

    Add email validation

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,46 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/login.module.css'
+import Image from 'next/image'
 
 const Login = () => {
 
-    const handleLoginWithEmail = () => {
-        console.log('clicked login')
+    const [email, setEmail] = useState('')
+    const [valid, setValid] = useState(false)
+    const [userMsg, setUserMsg] = useState('')
+
+    const validate = () => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
+    const handleError = (err) => {
+        setUserMsg(err)
+        setTimeout(() => {
+            setUserMsg('')
+        }, 2000);
     }
+
+    const handleLoginWithEmail = () => {
+        if (!valid) {
+            handleError('Invalid Email Address')
+        }
+    }
+
+    const handleChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    useEffect(() => {
+        if (validate()) {
+            setValid(true)
+        } else {
+            setValid(false)
+        }
+    }, [email])
 
     return (
         <main className={styles.container}>
@@ -16,8 +50,11 @@ const Login = () => {
             <div className={styles.loginUnderlay}>
                 <h2 className={styles.heading}>Welcome to Nextflix</h2>
                 <p className={styles.subHeading}>For the best experience login with your email address below</p>
-                <input type="text" placeholder='Email Address' className={styles.emailInput}></input>
-                <div className={styles.loginBtn} onClick={handleLoginWithEmail}>Login</div>
+                <div className={styles.emailWrapper}>
+                    <input type="text" placeholder='Email Address' value={email} onChange={handleChange} className={styles.emailInput}></input>
+                    <div>{valid && <Image src={'/static/icons/accept_check_white.png'} width="20px" height="20px" />}</div>
+                </div>
+                <div className={styles.loginBtn} onClick={handleLoginWithEmail}>{userMsg ? userMsg : 'Login'}</div>
                 <div className={styles.skip}>Skip login and enter site</div>
             </div>
         </main>
