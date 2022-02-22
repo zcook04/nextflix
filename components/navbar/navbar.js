dbf0b318557506b1a9939d0144849c23ae8dfd9b
Zachary Cook <zcook@ZC-MBAIR.local>
2022-02-21 23:03:18 -0500

    Add login logout butotns, add styling to navbar.  Update semantics

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Link from 'next/link'
 import styles from './navbar.module.css'
 
+
+
 function navbar() {
+    const [loggingOut, setLoggingOut] = useState(false)
+
+    const usernameHandler = () => {
+        loggingOut ? setLoggingOut(false) : setLoggingOut(true)
+    }
     return (
-        <nav className={styles.container}>
-            <div className={styles.navLeft} >Nextflix</div>
-            <div className={styles.navMiddle}>My Shows</div>
-            <div className={styles.navRight}>Log In</div>
-        </nav>
+        <header className={styles.container}>
+            <div className={styles.navLeft}><span className={styles.logo}>Nextflix</span></div>
+            <nav className={styles.navMiddle}>
+                <ul className={styles.navItems}>
+                    <li className={styles.navItem}>Home</li>
+                    <li className={styles.navItem}>My Shows</li>
+                </ul>
+            </nav>
+            <div className={styles.navRight}>
+                <div className={styles.username} onClick={usernameHandler}>
+                    zack@zack-cook.com
+                </div>
+                {loggingOut && <div className={styles.logout}>
+                    <Link href='/logout'>Logout</Link>
+                </div>}
+
+            </div>
+        </header>
     )
 }
 
