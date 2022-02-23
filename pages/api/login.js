a35fb7b1afb9aff426e36d1256a05da7f798351e
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-23 14:31:48 -0500

    Import Magic Admin and get metadata from /api/login bearer

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,16 @@
-export default function login(req, res) {
+import { mAdmin } from '../../lib/magic'
+
+export default async function login(req, res) {
     if (req.method !== 'POST')
         return res.status(422).send({ msg: 'Invalid method.  Method must be POST', done: false })
     if (!req.headers.authorization)
         return res.status(400).send({ msg: "Token is required", done: false })
 
-    const token = req.headers.authorization ? req.headers.authorization.substr(7) : ''
-
-    console.log(token)
-
     try {
+        const didToken = req.headers.authorization ? req.headers.authorization.substr(7) : ''
+        const metadata = await mAdmin.users.getMetadataByToken(didToken)
+        const { issuer, publicAddress, email } = metadata
+
         res.send({ done: true })
     } catch (error) {
         console.error('Something went wrong logging in', error)
