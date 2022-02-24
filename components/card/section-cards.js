6acb4f3e1aacf5328e49a676f8ca4d54f56efeb1
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-24 14:02:08 -0500

    Remove scroll from mylist

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,14 +1,16 @@
 import styles from './section-cards.module.css'
 import Card from './card'
 import Link from 'next/link'
+import clsx from 'classnames'
 
 function SectionCards(props) {
-    const { title, videos = [], size } = props
+    const { title, videos = [], size, shouldWrap = false } = props
+
 
     return (
         <section className={styles.container}>
             <h2 className={styles.title}>{title}</h2>
-            <div className={styles.cardWrapper}>
+            <div className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
                 {videos.map((video, i) => {
                     return (
                         <Link href={`/video/${video.id}`} key={video.id}><a>
---
+++ pages/browse/my-list.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Head from 'next/head'
+import SectionCards from '../../components/card/section-cards'
+import styles from '../../styles/mylist.module.css'
+import { getMyListVideos } from '../../lib/videos'
+import { verifyToken } from '../../lib/utils'
+
+export async function getServerSideProps(context) {
+    const token = context.req.cookies.token
+    const userId = await verifyToken(token)
+    if (!userId) {
+        return {
+            props: {},
+            redirect: {
+                destination: "/login",
+                permanent: false
+            }
+        }
+    }
+    const myListVideos = await getMyListVideos(userId, token)
+
+    return {
+        props: {
+            myListVideos
+        }
+    }
+}
+
+const myList = ({ myListVideos }) => {
+    console.log(myListVideos)
+    return (
+        <div>
+            <Head><title>Nextflix: My List</title></Head>
+            <section className={styles.sectionWrapper}>
+                <SectionCards title="My List: Liked Videos" videos={myListVideos} size="small" shouldWrap="true" />
+
+            </section>
+        </div>
+    )
+}
+
+export default myList
