3c5b9f98f7ec4b66fb7e2b9d94d2fdded1076067
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-23 23:50:58 -0500

    Add update and add functionility to stats api

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { findVideoByIssuer, addStatsOne } from '../../lib/hasura'
+import { findVideoByIssuer, addStatsOne, updateStatsOne } from '../../lib/hasura'
 
 const stats = async (req, res) => {
     if (req.method !== "POST")
@@ -9,23 +9,26 @@ const stats = async (req, res) => {
         if (!req.cookies.token)
             return res.status(403).json({ err: "Cookie required but not found" })
 
-        const { videoId } = req.query
+        const { videoId, watched, favorited } = req.body
         const { issuer } = jwt.verify(req.cookies.token, process.env.JWT_SECRET)
+
+        if (watched !== true || watched !== false)
+            return res.status(422).json({ done: false, err: 'Watched is required' })
+        if (favorited !== 0 || favorited !== 1 || favorited !== 2)
+            return res.status(422).json({ done: false, err: 'Incorrect Value for favorited' })
 
         const statsExist = await findVideoByIssuer(issuer, videoId, req.cookies.token)
 
         if (statsExist) {
-            //update
+            const updatedVideoStats = await updateStatsOne(req.cookies.token, { userId: issuer, videoId, watched, favorited })
+            return res.status(200).json({ done: true, updatedVideoStats })
         } else {
             const newVideoStats = await addStatsOne(req.cookies.token, issuer, videoId)
             return res.status(200).json({ done: true, newVideoStats })
         }
-
-        return res.status(200).json({ done: false, msg: 'No action taken.' })
-
     } catch (error) {
         console.log(error)
-        return res.status(500).json({ done: false, error: error.msg })
+        return res.status(500).json({ done: false, error })
     }
 
 }
