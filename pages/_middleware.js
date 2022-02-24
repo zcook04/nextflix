22d4735e9373f2b824e9aeaa631b9b8dafadf7dd
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2022-02-24 16:44:52 -0500

    Revert to next 12.0.9

---
+++ pages/_middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server"
+import { verifyToken } from "../lib/utils"
+
+export default async function middleware(req) {
+    const token = req ? req.cookies?.token : null
+    const userId = await verifyToken(token)
+
+    const { pathname } = req.nextUrl
+    const url = req.nextUrl.clone()
+
+    if (pathname.includes('/api/login') || userId || pathname.includes('/login') || pathname.includes('/static')) {
+        return NextResponse.next()
+    }
+
+    if (token && userId || pathname.includes('/api/login')) {
+        return NextResponse.next()
+    }
+
+    if (!token && pathname !== '/login') {
+        url.pathname = '/login'
+        return NextResponse.redirect(url)
+    }
+    return NextResponse.next()
+
+}
